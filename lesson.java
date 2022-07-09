a51fe3da253560110830bf5e4345024a230edd97
porcelain <evgeniiageyik@icloud.com>
2022-07-09 16:29:14 +0300

    123

--- lesson.java
+++ lesson.java
@@ -1,9 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class lesson {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int h = 0; // счетчик значения высоты моста
+        int b = 1; // мосты
+        int s = in.nextInt(); // ввод кол-ва мостов
 
-
-    int a = 10;
-    int b = 20;
-    int max = (a>b ? a : b );
-    System.out.println(max);
-}}
+        while (b < s) {
+            h = in.nextInt(); //  ввод значения высоты моста
+            if (h < 437) {
+                System.out.println("Врезался " + b);
+                break;
+            }
+            b++;
+        }
+        if (h >437) {
+            System.out.println("No Crash"+ b);
+        }
+    }
+}
